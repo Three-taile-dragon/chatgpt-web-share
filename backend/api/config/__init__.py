6de6949777de6589efde52d7d80c0260b7350407
Dragonsss <47767754+Three-taile-dragon@users.noreply.github.com>
2023-03-25 23:43:35 +0800

    Update __init__.py
    add: read env config

--- backend/api/config/__init__.py
+++ backend/api/config/__init__.py
@@ -27,6 +27,16 @@ class Config:
         with open(config_file, 'w') as f:
             yaml.dump(self.config, f)
 
-
+def replace_env_variables(config):
+    for key, value in config.items():
+        if isinstance(value, str):
+            config[key] = value.replace('{{', '{').replace('}}', '}').format(**os.environ)
+        elif isinstance(value, dict):
+            config[key] = replace_env_variables(value)
+    return config
+            
+            
+           
 config_file = os.path.join(os.path.dirname(__file__), "config.yaml")
 config = Config(config_file)
+config = replace_env_variables(config)
